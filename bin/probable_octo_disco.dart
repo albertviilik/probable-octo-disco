9422bf25cabeaefe900b1edd24b18d41636bc725
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-03-06 15:52:35 +0000

    Initial Commit

---
+++ bin/probable_octo_disco.dart
@@ -0,0 +1,5 @@
+import 'package:probable_octo_disco/probable_octo_disco.dart' as probable_octo_disco;
+
+void main(List<String> arguments) {
+  print('Hello world: ${probable_octo_disco.calculate()}!');
+}
---
+++ test/probable_octo_disco_test.dart
@@ -0,0 +1,8 @@
+import 'package:probable_octo_disco/probable_octo_disco.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
