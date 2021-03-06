7976ddecbbdbbfbde772a6d41d8d4d12181ab08b
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-03-06 17:38:17 +0000

    Update probable_octo_disco.dart
    Added a config map that enables setting the HOST and PORT before compiling. Does not require passing arguments anymore.

--- bin/probable_octo_disco.dart
+++ bin/probable_octo_disco.dart
@@ -1,6 +1,16 @@
 import 'package:probable_octo_disco/probable_octo_disco.dart'
     as probable_octo_disco;
 
+final config = {
+  'HOST': 'localhost',
+  'PORT': '1234',
+};
+
 void main(List<String> arguments) async {
-  await probable_octo_disco.main(arguments);
+  await probable_octo_disco.main([
+    '-a',
+    config['HOST'],
+    '-p',
+    config['PORT'],
+  ]);
 }
